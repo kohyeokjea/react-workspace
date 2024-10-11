0739edda4e1a6a2a295a93c042aab8e18e99d14b
kohyeokjea <kohj0686@naver.com>
2024-10-11 21:51:19 +0900

    React 11일차

---
+++ ex13/1302-todolist-svg/src/TodoItem.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import "./assets/css/TodoItem.css"
+import { ReactComponent as CheckIcon } from "./assets/svg/check.svg";
+import { ReactComponent as DeleteIcon } from "./assets/svg/delete.svg";
+
+function TodoItem({ id, done, text }) {
+    const [hoveredDelete, setHoveredDelete] = useState(false);
+    return (
+        <div className="todo-item-block">
+            <div className={`check-circle ${done ? "done" : ""}`}>
+                {
+                    done && <CheckIcon fill='#38d9a9' />
+                }
+            </div>
+            <div className={`text ${done && "done"}`}>{text}</div>
+            <div className='remove'>
+                <DeleteIcon
+                    onMouseEnter={() => setHoveredDelete(true)}
+                    onMouseLeave={() => setHoveredDelete(false)}
+                    fill={hoveredDelete ? "#ff6b6b" : "dee2e5"} />
+            </div>
+        </div>
+    )
+}
+
+export default TodoItem
